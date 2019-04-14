45fb7070b605bdb36419ab11da469fc90b626886
nurinamu <nurinamu@gmail.com>
2019-04-14 13:58:15 +0900

    Refactor internal structure

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,20 @@ import 'todo.dart';
 void main() => runApp(MyApp());
 
 // This widget is the root of your application.
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final List<Text> todos = <Text>[];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "nurinamu",
-      home: TodoWidget()
+      home: TodoWidget(todos)
     );
   }
 }
--- lib/todo.dart
+++ lib/todo.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 
 class TodoWidget extends StatefulWidget {
+  final List<Text> _todos;
+  TodoWidget(this._todos);
+
   @override
-  State<StatefulWidget> createState() => TodoList();
+  State<StatefulWidget> createState() => TodoList(_todos);
 }
 
 class TodoList extends State<TodoWidget> {
-  final List<Text> todos = <Text>[];
-
+  final List<Text> _todos;
+  TodoList(this._todos);
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +20,9 @@ class TodoList extends State<TodoWidget> {
       ),
       body: todoList(),
       floatingActionButton: FloatingActionButton(
-          onPressed: null,
+          onPressed: () =>  {
+            Navigator.push(context, MaterialPageRoute(builder: null))
+      },
         child: Icon(Icons.add),
       ),
     );
@@ -25,10 +30,10 @@ class TodoList extends State<TodoWidget> {
 
   Widget todoList() {
     return
-      todos.isEmpty ?
+      _todos.isEmpty ?
       Text("할일이 없다.") :
       ListView.builder(itemBuilder: (BuildContext _context, int i){
-        return todos[i];
+        return _todos[i];
       });
   }
 
